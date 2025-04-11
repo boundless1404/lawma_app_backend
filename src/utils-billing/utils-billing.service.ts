7cfc8a6582fe09905906b0cfb455e6e4ba91c76c
akonihakeem <akozymayor@gmail.com>
2025-04-11 16:30:12 +0100

    reversed deduction amount to 4%

--- src/utils-billing/utils-billing.service.ts
+++ src/utils-billing/utils-billing.service.ts
@@ -2179,7 +2179,7 @@ export class UtilsBillingService {
         const chargedAmount = data.amount;
 
         // deduct boundless fees
-        const boundelsssDeductionPercentage = 0.06; // 6%
+        const boundelsssDeductionPercentage = 0.04; // 4%
         const boundelsssDeductionPercentageAmount =
           boundelsssDeductionPercentage * chargedAmount;
 
