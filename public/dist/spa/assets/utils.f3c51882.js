250888d95e9279c3365ddf8f7b255e831f062fe2
boundless1404 <147217504+boundless1404@users.noreply.github.com>
2025-06-23 18:35:04 +0100

    Revert "Revert "Feat/update billing service""

--- public/dist/spa/assets/utils.f3c51882.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index.e12bd1bb.js";import{v as r,g as i}from"./utils.01331f21.js";import{g as l}from"./get-css-var.23548d6a.js";var a={install:e=>{e.config.globalProperties.$getColor=o=>l(o)}},f=t(({app:e})=>{e.config.globalProperties.$validateField=r,e.config.globalProperties.$getHighestZIndex=i,e.use(a)});export{f as default};
